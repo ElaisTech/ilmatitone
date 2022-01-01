19ba25cdfc9429f8c2b147b8dd5cd828f10ad8e7
ElaisTech <elazzaro50@gmail.com>
2022-01-01 12:39:37 +0100

    2.0
    Aggiunto tasto menu.
    Nascosto il menu con display: none.
    Funzione javascript mostra menu ma non lo nasconde

---
+++ index.js
@@ -0,0 +1,10 @@
+var toggle = () => {
+    
+    if (document.querySelector('.panel').style.display = "none") {
+         document.querySelector('.panel').style.display = "flex";
+    } else if (document.querySelector('.panel').style.display = "flex") {
+        document.querySelector('.panel').style.display = "none";
+    }
+}
+
+
